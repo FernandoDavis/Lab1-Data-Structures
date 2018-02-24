27bfd11b39c58237256f0be0575286dcff8b79eb
FernandoDavis <fernando.davis@upr.edu>
2018-02-24 15:37:25 -0400

    Area calculation

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args){
+		double r = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input radius of circle: ");
+		r = input.nextDouble();
+		double area = Math.PI * Math.pow(r,2);
+		System.out.println("The area of the circle with radius: " + r + " is: " + area);
+	}
+}
