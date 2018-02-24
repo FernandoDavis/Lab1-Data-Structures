3f29a0f14088f7955114bf49df862a2304359a0f
FernandoDavis <fernando.davis@upr.edu>
2018-02-24 15:39:02 -0400

    Revert "Added permiter feature"
    This reverts commit 34b9ac4f1d603e9f94d15849bc30a946ee8dcf34.

--- Main.java
+++ Main.java
@@ -7,6 +7,5 @@ public class Main{
 		r = input.nextDouble();
 		double area = Math.PI * Math.pow(r,2);
 		System.out.println("The area of the circle with radius: " + r + " is: " + area);
-		System.out.println("The half area of the circle with radius: " + r + " is: " + area/2);
 	}
 }
